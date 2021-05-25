7496dac3316a63502c60af28d0ad36535fa448dc
crono32 <crono32@protonmail.com>
2021-05-25 18:22:04 +0100

    Create calculator design

--- main.js
+++ main.js
@@ -22,15 +22,46 @@ function operate(operator, a, b) {
   switch (operator) {
     case "+":
       return add(a, b);
-      break;
     case "-":
       return subtract(a, b);
-      break;
     case "*":
       return multiply(a, b);
-      break;
     case "/":
       return divide(a, b);
-      break;
   }
 }
+
+function createButton(id, gridArea, text) {
+  let btn = document.createElement("button");
+  btn.id = id;
+  btn.style.gridArea = gridArea;
+  btn.textContent = text;
+  return btn;
+}
+
+const buttons = document.querySelector("#buttons");
+let nums = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+for (let i = 0; i < 10; i++) {
+  let id = `btn-${i}`;
+  let gridArea = `${nums[i]}`;
+  buttons.appendChild(createButton(id, gridArea, `${i}`));
+}
+buttons.appendChild(createButton("btn-del", "del", "DEL"));
+buttons.appendChild(createButton("btn-ac", "ac", "AC"));
+buttons.appendChild(createButton("btn-multiply", "multiply", "×"));
+buttons.appendChild(createButton("btn-divide", "divide", "÷"));
+buttons.appendChild(createButton("btn-add", "add", "+"));
+buttons.appendChild(createButton("btn-subtract", "subtract", "−"));
+buttons.appendChild(createButton("btn-equals", "equals", "="));
+buttons.appendChild(createButton("btn-decimalPoint", "decimalPoint", "."));
